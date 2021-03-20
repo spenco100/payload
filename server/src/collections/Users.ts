6cd37bce45c8d7a5352ddc03b97cdeea68237133
spenco100 <spencers9478@gmail.com>
2021-03-19 20:34:14 -0500

    initial structuring

---
+++ server/src/collections/Users.ts
@@ -0,0 +1,34 @@
+import { BeforeReadHook } from 'payload/dist/collections/config/types';
+import { CollectionConfig } from 'payload/types';
+
+const onlyNameIfPublic: BeforeReadHook = ({ req: { user }, doc }) => {
+  // Only return name if not logged in
+  if (!user) {
+    return { name: doc.name };
+  }
+  return doc;
+};
+
+const Users: CollectionConfig = {
+  slug: 'users',
+  auth: true,
+  admin: {
+    useAsTitle: 'email',
+  },
+  access: {
+    read: () => true,
+  },
+  hooks: {
+    beforeRead: [onlyNameIfPublic]
+  },
+  fields: [
+    // Email added by default
+    {
+      name: 'name',
+      label: 'Name',
+      type: 'text',
+    }
+  ],
+};
+
+export default Users;
